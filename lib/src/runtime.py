1eb82b200888e9826aca6fdd6c2409f4980a6732
Sanjay <Sanjay-B@users.noreply.github.com>
2017-09-22 23:05:41 -0400

    removed quotes

--- lib/src/runtime.py
+++ lib/src/runtime.py
@@ -3,13 +3,6 @@ from std.parser import *
 from std.lexer import *
 
 
-'''
-def run():
-    data = open_file(argv[1])
-    toks = lex(data)
-    parse(toks)
-'''
-
 def process():
 	#raw = open_f(argv[1])
 	data = lex(argv[1])
