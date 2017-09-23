ee45fe5232721b7c3b8d10103f398cd62857c56f
Sanjay <Sanjay-B@users.noreply.github.com>
2017-09-23 19:54:56 -0400

    Added System class & Added Various methods to Systematic Class
    - Added useful methods
    
    
    *not fully discussed due to security reasons*

--- lib/src/std/system.py
+++ lib/src/std/system.py
@@ -1,4 +1,34 @@
-#Python-Core Imports
+# -*- coding: utf-8 -*-
+#
+#  system.py
+#  Iris
+#
+#  By Sanjay-B(Sanjay Bhadra)
+#  Copyright © 2017 Sage Technologies LLC. All rights reserved.
+#
+
+"""
+The MIT License (MIT)
+
+Copyright © 2017 Sage Technologies LLC. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, modify, merge, publish and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
 from sys import *
 import time 
 
@@ -11,6 +41,20 @@ class System:
         data = open(filename, "r").read()
         data += "<EOF>"
         return data
+
+    def vList(_list):
+        for v in _list:
+            a = v
+            yield a
+            del a
+
+    def iList(_list):
+        for i in _list:
+            return i
+
+    def bList(_list):
+        for b in _list:
+            return b
 
 class Systematic:
     def vAssign(name,value):
@@ -28,21 +72,10 @@ class Systematic:
     def aQuote(name):
         return "'"+name+"'"
 
+    def _loop(_array):
+        for item in _array:
+            return item
 
-
-'''
-def doASSIGN(varname, varvalue):
-    symbols[varname[7:]] = varvalue
-
-
-
-def getVARIBLE(varname):
-    varname = varname[7:]
-    if varname in symbols:
-        #print("TRUE")
-        return symbols[varname]
-    else:
-        return Error_List[9]
-        time.sleep(1)
-        exit() #Stop current process
-'''
+    def _print(_obj):
+        a = str(_obj)
+        print(a)
