67c75bd84b83320a669a580bea169225aec63c67
Sanjay <Sanjay-B@users.noreply.github.com>
2018-01-10 08:58:31 -0500

    [Strings] Added SubStr method

--- strings/string.py
+++ strings/string.py
@@ -9,22 +9,23 @@ def text_to_binary(text) :
 def text_to_hex(string):
     return ''.join(format(ord(c), 'x') for c in string)
 
-def uppercase (string):
+def uppercase(string):
     return string.upper()
 
-def lowercase (string) :
+def lowercase(string):
     return string.lower()
 
 def length(string):
      return sum(map(lambda x:1, string))
 
-
-def replace (string, old, new):
+def replace(string, old, new):
     return string.replace(old, new)
 
-def slice (start, stop, step):
-    return slice(start, stop, step)
+def slice(start, stop, step):
+    return slice(start,stop,step)
 
-def concat (string1, string2) :
+def concat (string1, string2):
     return ' '.join([string1, string2])
 
+def substr(string,start,stop):
+    return string.substring(start,stop)
